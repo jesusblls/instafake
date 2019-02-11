7342d3bb4dfb2ec20d576d969ceddc6442e92a50
Jesus Castillo Hernandez <jesus.castilloh@stkprojects.com>
2019-02-11 10:56:50 -0600

    Updates

---
+++ precache-manifest.589d527b7efd563bcb610387f42bced8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/instafake/robots.txt"
+  },
+  {
+    "revision": "c356560d655c02e02d1c",
+    "url": "/instafake/js/chunk-vendors.a1cf09be.js"
+  },
+  {
+    "revision": "68d1051a2b951cbcf729",
+    "url": "/instafake/js/app.08ff235b.js"
+  },
+  {
+    "revision": "0e1ac08f7578f98deb5c",
+    "url": "/instafake/js/about.93810699.js"
+  },
+  {
+    "revision": "758c6ba2383944670c939ceffe981b53",
+    "url": "/instafake/index.html"
+  },
+  {
+    "revision": "68d1051a2b951cbcf729",
+    "url": "/instafake/css/app.ca24ced2.css"
+  }
+];
